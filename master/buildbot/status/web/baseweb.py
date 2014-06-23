5384a30f2f774258647fdf6cbb387e7a385776dc
Simon Holm Nielsen <simon@unity3d.com>
2014-06-23 12:15:08 +0200

    branch comparison feature

--- master/buildbot/status/web/baseweb.py
+++ master/buildbot/status/web/baseweb.py
@@ -46,6 +46,7 @@ from buildbot.status.web.auth import AuthFailResource,AuthzFailResource, LoginRe
 from buildbot.status.web.root import RootPage
 from buildbot.status.web.users import UsersResource
 from buildbot.status.web.change_hook import ChangeHookResource
+from buildbot.status.web.branch_comparison import BranchComparisonResource
 
 # this class contains the WebStatus class.  Basic utilities are in base.py,
 # and specific pages are each in their own module.
@@ -378,6 +379,7 @@ class WebStatus(service.MultiService):
         self.putChild("logout", LogoutKatanaResource())
         self.putChild("login", LoginKatanaResource())
         self.putChild("forms", FormsKatanaResource())
+        self.putChild("branch_comparison", BranchComparisonResource())        
 
 
     def __repr__(self):
